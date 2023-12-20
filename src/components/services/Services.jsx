bb972e86d36470092470f8f4bd00eba677eb43ea
Hakkeshawa <aleks.work@yahoo.com>
2023-12-20 16:28:42 +0800

    Updated Website

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,104 @@
+import { useRef } from "react";
+import "./services.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+      animate={"animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on helping your brand grow
+          <br /> and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{color:"orange"}}>For Your</motion.b> Business.
+          </h1>
+          <button>WHAT CAN I DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Creating a prototype for you website and banner</h2>
+          <p>
+            I can create prototypes for websites and banners in Figma, prototype can contain different pages, and will have a responsive design
+          </p>
+          
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Developing a Website (Frontend and Backend)</h2>
+          <p>
+            I can create a fully functionate website, with Frontend on React/HTML,CSS,JS and Backend with Django, .Net
+          </p>
+          
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Developing Applications</h2>
+          <p>
+            I can create an app using C++, C#, Python.
+          </p>
+          
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Gamedev</h2>
+          <p>
+            I can also work in game industry. RenPy and Unity are my main platforms in gamedev.
+          </p>
+          
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
