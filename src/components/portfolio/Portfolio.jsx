ed140c4505f50ef093e2c03486e2bf02e94cac36
Hakkeshawa <149156344+Hakkeshawa@users.noreply.github.com>
2024-10-31 21:59:24 +0800

    Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,26 +3,21 @@ import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
-  {
+    {
     id: 1,
+    title: "Analog of Meets
+    img: "google-meet-audio-locks.png",
+    desc: "Made by using GoLang + JS",
+  },
+  {
+    id: 2,
     title: "Telegram bots, made on python (Cafe bot, Weather bot, Horoscope bot, Music bot )",
     img: "bot.jpeg",
     desc: "Made by using library telebot",
   },
-  {
-    id: 2,
-    title: "Prototypes of websites in Figma",
-    img: "Desktop - 1.png",
-    desc: "Made in Figma",
-  },
+
   {
     id: 3,
-    title: "This Website",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Made by using React JS",
-  },
-  {
-    id: 4,
     title: "And a lot more projects",
     img: "git.png",
     desc: "Visit my GitHub for more information",
